0bf6fa0a6651b9a73efe8148678f1d9566bd29fc
Nathanael Adam <nathanaeladam3@gmail.com>
2021-04-21 17:56:27 +0200

    commit

--- script.js
+++ script.js
@@ -80,3 +80,20 @@ let questions = [
         "right_answer": "4",
     }
 ];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('question-count').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer1').innerHTML = question['answer1'];
+    document.getElementById('answer2').innerHTML = question['answer2'];
+    document.getElementById('answer3').innerHTML = question['answer3'];
+    document.getElementById('answer4').innerHTML = question['answer4'];
+}
